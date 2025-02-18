463ca0a52f2f9ec7102277187d4d53da88002ce2
Pushpendra Jakhar <notoriousdracula1@gmail.com>
2025-02-18 22:45:41 +0530

    18 feb problems pt 2

---
+++ 518b.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <string>
+#include <unordered_map>
+#include <queue>
+#include <map>
+#include <set>
+#include <cstring>
+#include <tuple>
+#include <stack>
+
+using namespace std;
+
+#define vi vector<int>
+#define vl vector<ll>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define F first
+#define S second
+#define sz size()
+#define NL cout << "\n";
+#define loop(i, a, b) for(int i=a; i<b; i++)
+#define loopr(i, a, b) for(int i=a; i>b; i--)
+#define geta(A, a, b) for(int i=a; i<b; i++){cin >> A[i];}
+#define getv(v, a, b) for(int i=a; i<b; i++){int x; cin >> x; v.push_back(x);}
+#define gets(s, a, b) for(int i=a; i<b; i++){int x; cin >> x; s.insert(x);}
+#define seea(A, a, b) for(int i=a; i<b; i++){cout << A[i]<<" ";}cout <<"\n";
+
+// template<typename... T> 
+// void put(T&... args){
+//     cout << a << " ";
+// }
+template<typename... T>
+void get(T&... args) { ((cin >> args), ...);}
+template<typename... T>
+void put(T&&... args) { ((cout << args << " "), ...);}
+template<typename... T>
+void putl(T&&... args) { ((cout << args << " "), ...); cout<<'\n';}
+
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9+7;
+const ll inf = 1LL<<60;
+const ld ep = 0.0000001;
+const ld pi = acos(-1.0);
+
+
+int main(){
+    string s, t;
+    cin >> s;
+    cin >> t;
+    map<char, int> dict;
+    ll yayCnt=0, whoopsCnt=0;
+    for(auto c: t){
+        dict[c]++;
+    }
+    for(auto &c: s){
+        if(dict[c]>0){
+            yayCnt++;
+            dict[c]--;
+            c = '.';
+        } 
+    }
+    for(auto &c: s){
+        if(c!='.' && c>='a' && dict[char(c-32)]>0){
+            whoopsCnt++;
+            dict[char(c-32)]--;
+        }
+        if(c!='.' && c<'a' && dict[char(c+32)]>0){
+            whoopsCnt++;
+            dict[char(c+32)]--;
+        }
+    }
+    cout << yayCnt << " " << whoopsCnt << endl;
+    return 0;
+}
