eccf50fc6310dac83b77b05005d6c06465c2d539
Pushpendra Jakhar <pjakhar@iitb.ac.in>
2025-03-08 16:44:18 +0530

    8 march problems

---
+++ 577c.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <string>
+#include <unordered_map>
+#include <queue>
+#include <map>
+#include <set>
+#include <cstring>
+#include <tuple>
+#include <stack>
+#include <iomanip>
+
+using namespace std;
+
+#define vi vector<int>
+#define vl vector<ll>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define F first
+#define S second
+#define sz size()
+#define NL cout << "\n";
+#define loop(i, a, b) for(int i=a; i<b; i++)
+#define loopr(i, a, b) for(int i=a; i>b; i--)
+#define geta(A, a, b) for(int i=a; i<b; i++){cin >> A[i];}
+#define getv(v, a, b) for(int i=a; i<b; i++){int x; cin >> x; v.push_back(x);}
+#define gets(s, a, b) for(int i=a; i<b; i++){int x; cin >> x; s.insert(x);}
+#define seea(A, a, b) for(int i=a; i<b; i++){cout << A[i]<<" ";}cout <<"\n";
+
+// template<typename... T> 
+// void put(T&... args){
+//     cout << a << " ";
+// }
+template<typename... T>
+void get(T&... args) { ((cin >> args), ...);}
+template<typename... T>
+void put(T&&... args) { ((cout << args << " "), ...);}
+template<typename... T>
+void putl(T&&... args) { ((cout << args << " "), ...); cout<<'\n';}
+
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9+7;
+const ll inf = 1LL<<60;
+const ld ep = 0.0000001;
+const ld pi = acos(-1.0);
+
+
+int main(){
+	int n;
+	vector<bool> seive(10002,true);
+    cin >> n;
+    vector<int> primes;
+    for(int i=2; i*i<=10000; i++){
+        if(seive[i]){
+            for(int p=i*2; p<=10000; p+=i)
+                seive[p] = false;
+        }
+    }
+    for(int i=2; i<=10000; i++){
+        if(seive[i])
+            primes.push_back(i);
+    }
+    vector<int> res;
+    int i=0;
+    ll p = primes[i];
+    while(p<=n){
+        while(p<=n){
+            res.push_back(p);
+            p = p*primes[i];
+        }
+        i++;
+        p = primes[i];
+    }
+    cout << res.size() << endl;
+    seea(res, 0, res.size());
+    return 0;
+
+}
---
+++ 71c.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <string>
+#include <unordered_map>
+#include <queue>
+#include <map>
+#include <set>
+#include <cstring>
+#include <tuple>
+#include <stack>
+#include <iomanip>
+
+using namespace std;
+
+#define vi vector<int>
+#define vl vector<ll>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define F first
+#define S second
+#define sz size()
+#define NL cout << "\n";
+#define loop(i, a, b) for(int i=a; i<b; i++)
+#define loopr(i, a, b) for(int i=a; i>b; i--)
+#define geta(A, a, b) for(int i=a; i<b; i++){cin >> A[i];}
+#define getv(v, a, b) for(int i=a; i<b; i++){int x; cin >> x; v.push_back(x);}
+#define gets(s, a, b) for(int i=a; i<b; i++){int x; cin >> x; s.insert(x);}
+#define seea(A, a, b) for(int i=a; i<b; i++){cout << A[i]<<" ";}cout <<"\n";
+
+// template<typename... T> 
+// void put(T&... args){
+//     cout << a << " ";
+// }
+template<typename... T>
+void get(T&... args) { ((cin >> args), ...);}
+template<typename... T>
+void put(T&&... args) { ((cout << args << " "), ...);}
+template<typename... T>
+void putl(T&&... args) { ((cout << args << " "), ...); cout<<'\n';}
+
+typedef long long ll;
+typedef long double ld;
+
+const ll mod = 1e9+7;
+const ll inf = 1LL<<60;
+const ld ep = 0.0000001;
+const ld pi = acos(-1.0);
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<ll> arr(n);
+    geta(arr, 0, n);
+    map<int, set<int>> factors;
+    for(int i=1; i<=n/3; i++){
+        if(n%i==0){
+            factors[i].insert(i);
+        }
+    }
+    for(int i=0; i<n; i++){
+        if(arr[i]==0){
+            vector<int> to_delete;
+            for(auto &fi: factors){
+                fi.second.insert((i+1)%fi.first);
+                if(fi.second.size()>fi.first){
+                    to_delete.push_back(fi.first);
+                }
+            }
+            for(auto t: to_delete){
+                factors.erase(factors.find(t));
+            }
+        }
+
+    }
+    // for(auto fi: factors){
+    //     cout << fi.first << " : ";
+    //     for(auto fii: fi.second)
+    //         cout << fii << " ";
+    //     cout << endl;
+    // }
+    bool res = false;
+    for(auto fi: factors){
+        if(fi.second.size()<=fi.first){
+            res = true;
+            break;
+        }
+    }
+    if(res)
+        cout << "YES\n";
+    else 
+        cout << "NO\n";
+    return 0;
+}
